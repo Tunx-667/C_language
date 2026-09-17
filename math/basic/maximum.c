cb884544e87b4afa77bcca6d337c1b3379b96f85
Nguyễn Tiến Dũng (Tunx) <nguyentiendungg25112007@gmail.com>
2026-09-17 13:17:00 +0700

    Add files via upload

---
+++ math/basic/maximum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main (){
+  int a;
+  int b;
+  scanf("%d %d", &a ,&b) ;
+  if (a>b){
+    printf("%d\n",a);
+  }
+  if (a<b){
+    printf("%d\n",b);
+  }
+  else{
+    printf("%d\n",a);
+  }
+  return 0;
+}
---
+++ math/basic/radius of a circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+  double r;
+  scanf("%lf", &r);
+   printf ("%2.lf\n",2*r);
+   printf ("%2.lf\n",2*r*M_PI);
+   printf ("%2.lf\n",r*r*M_PI);
+
+    return 0;
+
+}
